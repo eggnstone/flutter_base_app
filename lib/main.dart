dc57e9b6a44b4657561352f8c528b65f8053f51f
Mark Eggenstein <>
2020-06-27 18:22:14 +0200

    Added logger.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:eggnstone_flutter/eggnstone_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_base/App.dart';
+import 'package:get_it/get_it.dart';
 
 void main()
 {
     try
     {
+        // Create a logger service that is switched on for debug builds.
+        var logger = LoggerService(DartTools.isDebugBuild());
+        GetIt.instance.registerSingleton<LoggerService>(logger);
+        logger.logDebug('Logger started.');
+
         runApp(App());
     }
     catch (e, stackTrace)
